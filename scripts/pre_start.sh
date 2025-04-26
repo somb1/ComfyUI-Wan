0616a8603600c915762ca881bac4211b151b3343
somb1 <somersbyappl@gmail.com>
2025-04-26 23:08:23 +0900

    Edit

--- scripts/pre_start.sh
+++ scripts/pre_start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+export PYTHONUNBUFFERED=1
+
+#echo "**** Setting the timezone based on the TIME_ZONE environment variable. If not set, it defaults to Etc/UTC. ****" && \
+export TZ=${TIME_ZONE:-"Etc/UTC"} && \
+echo "**** Timezone set to $TZ ****" && \
+echo "$TZ" | sudo tee /etc/timezone > /dev/null && \
+sudo ln -sf "/usr/share/zoneinfo/$TZ" /etc/localtime && \
+sudo dpkg-reconfigure -f noninteractive tzdata
+
 echo "**** syncing venv to workspace, please wait. This could take a while on first startup! ****"
 rsync -au --remove-source-files /venv/ /workspace/venv/ && rm -rf /venv
 # Updating '/venv' to '/workspace/venv' in all text files under '/workspace/venv/bin'
@@ -25,10 +34,3 @@ for dir in /comfyui-models/*/; do
     name=$(basename "$dir")
     ln -sf "$dir" "/workspace/ComfyUI/models/$name"
 done
-
-#echo "**** Setting the timezone based on the TIME_ZONE environment variable. If not set, it defaults to Etc/UTC. ****" && \
-export TZ=${TIME_ZONE:-"Etc/UTC"} && \
-echo "**** Timezone set to $TZ ****" && \
-echo "$TZ" | sudo tee /etc/timezone > /dev/null && \
-sudo ln -sf "/usr/share/zoneinfo/$TZ" /etc/localtime && \
-sudo dpkg-reconfigure -f noninteractive tzdata
