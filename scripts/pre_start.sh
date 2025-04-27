026884faeb8a6cfa28f36cd45bc4e1d4af7274b3
somb1 <somersbyappl@gmail.com>
2025-04-27 14:04:46 +0900

    edit

--- scripts/pre_start.sh
+++ scripts/pre_start.sh
@@ -2,11 +2,11 @@
 
 export PYTHONUNBUFFERED=1
 
-echo "**** Setting the timezone based on the TIME_ZONE environment variable. If not set, it defaults to Etc/UTC. ****" && \
-export TZ=${TIME_ZONE:-"Etc/UTC"} && \
-echo "**** Timezone set to $TZ ****" && \
-echo "$TZ" | sudo tee /etc/timezone > /dev/null && \
-sudo ln -sf "/usr/share/zoneinfo/$TZ" /etc/localtime && \
+echo "**** Setting the timezone based on the TIME_ZONE environment variable. If not set, it defaults to Etc/UTC. ****"
+export TZ=${TIME_ZONE:-"Etc/UTC"}
+echo "**** Timezone set to $TZ ****"
+echo "$TZ" | sudo tee /etc/timezone > /dev/null
+sudo ln -sf "/usr/share/zoneinfo/$TZ" /etc/localtime
 sudo dpkg-reconfigure -f noninteractive tzdata
 
 echo "**** syncing venv to workspace, please wait. This could take a while on first startup! ****"
